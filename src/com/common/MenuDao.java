6f89314c091577aa30c1780de3ddffafde6bf43d
ZX <ZX@ZX-PC>
2016-06-01 20:47:30 +0800

    完成首页自定义办公桌功能，用户只能选择具有权限的菜单，展示排序按照选择的先后顺序

--- src/com/common/MenuDao.java
+++ src/com/common/MenuDao.java
@@ -2,12 +2,19 @@ package com.common;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import org.apache.commons.lang.StringUtils;
+import org.hibernate.Hibernate;
+import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
+import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.access.AccessDeniedException;
@@ -21,6 +28,7 @@ import com.bstek.bdf2.core.model.Url;
 import com.bstek.bdf2.core.security.SecurityUtils;
 import com.bstek.bdf2.core.security.UserAuthentication;
 import com.bstek.dorado.annotation.DataProvider;
+import com.bstek.dorado.annotation.Expose;
 
 public class MenuDao extends CoreHibernateDao implements InitializingBean{
 	public static final String BEAN_ID="menuDao";
@@ -73,7 +81,103 @@ public class MenuDao extends CoreHibernateDao implements InitializingBean{
 			} catch (AccessDeniedException ex) {}
 		}
 	}
+	//自定义我的办公桌
+	@SuppressWarnings({ "unchecked", "deprecation" })
+	@DataProvider
+	public Collection<Url> loadMyMeunUrls() {
+		IUser user = ContextHolder.getLoginUser();
+		if (user == null) {
+			throw new NoneLoginException("Please login first");
+		}
+		List<Url> cacheUrls=(List<Url>)this.applicationCache.getCacheObject(URL_FOR_NAVI_CACHE_KEY);
+		UserAuthentication authentication = new UserAuthentication(user);
+		Collection<Url> result = new ArrayList<Url>();
+		authorityCheck2(cacheUrls,authentication,result);//获取有权限的菜单集合
+		Session session = this.getSessionFactory().openSession();
+		List<String> urls = null;
+		try {
+			String sql = "select Url from Sys_MyDesk where Username='"+user.getUsername()+"'";
+			Query query = session.createSQLQuery(sql).addScalar("Url",Hibernate.STRING);// 设置返回值类型，不然会报错
+			urls = query.list();//获取已设置的办公桌URL
+	    } finally {
+		    session.flush();
+		    session.close();
+		}
+		for(Url url:result){//已设置的菜单进行标记
+			if(urls.isEmpty() || !urls.contains(url.getUrl()))
+				url.setForNavigation(false);
+		}
+		return result;
+	}
 	
+	private void authorityCheck2(Collection<Url> urls,UserAuthentication authentication,Collection<Url> result){
+		for (Url url : urls) {
+			String targetUrl = url.getUrl();
+			List<Url> children=url.getChildren();
+			int childrenCount = 0;
+			if(children!=null){
+				childrenCount=children.size();
+			}
+			if (childrenCount==0 && StringUtils.isEmpty(targetUrl)) {
+				continue;
+			}
+			if(StringUtils.isEmpty(targetUrl)){
+				targetUrl = url.getName();				
+			}
+			try {
+				SecurityUtils.checkUrl(authentication, targetUrl);
+				if(!StringUtils.isEmpty(url.getUrl())){
+				    Url newUrl=buildNewUrl(url);
+				    result.add(newUrl);
+				}
+				if(children!=null){
+					authorityCheck2(children,authentication,result);				
+				}
+			} catch (AccessDeniedException ex) {}
+		}
+	}
+	//获取我的办公桌
+	@SuppressWarnings("unchecked")
+	@Expose
+	public List<Map<String, Object>> getMyDesk() throws Exception{
+		IUser user = ContextHolder.getLoginUser();
+		if (user == null) {
+			throw new NoneLoginException("Please login first.");
+		}
+		Session session = this.getSessionFactory().openSession();
+		try {
+		    String sql = "select url,name,'>images/home/MyDesk.png' image from Sys_MyDesk where Username='"+user.getUsername()+"' order by Sequence";
+		    Query query = session.createSQLQuery(sql).setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
+		    List<Map<String, Object>> list = query.list();
+		    return list;
+	    } finally {
+		    session.flush();
+		    session.close();
+		}
+	}
+	//保存我的办公桌
+	@Expose
+	public void setMyDesk(Collection<Url> urls) throws Exception{
+		IUser user = ContextHolder.getLoginUser();
+		if (user == null) {
+			throw new NoneLoginException("Please login first");
+		}
+		Session session = this.getSessionFactory().openSession();
+		try {
+			String sql = "delete from Sys_MyDesk where Username='"+user.getUsername()+"'";
+            session.createSQLQuery(sql).executeUpdate();
+            int i=1;
+            for(Url url:urls ){
+            	String oid = UUID.randomUUID().toString();
+            	String sql0 = "insert into Sys_MyDesk('Oid','Username','Url','name','Sequence') values (''{0}'',''{1}'',''{2}'',''{3}'',{4})";
+            	String sql1 = MessageFormat.format(sql0,oid,user.getUsername(),url.getUrl(),url.getName(),i++);
+            	session.createSQLQuery(sql1).executeUpdate();
+            }
+	    } finally {
+		    session.flush();
+		    session.close();
+		}
+	}
 	public ApplicationCache getApplicationCache() {
 		return applicationCache;
 	}
